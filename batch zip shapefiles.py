e7d8282d88e124c0a2f0cb44c3201e377efa3218
Daniel Wiet <danwiet@gmail.com>
2021-01-19 14:37:56 -0600

    format with correct indents

--- batch zip shapefiles.py
+++ batch zip shapefiles.py
@@ -37,34 +37,34 @@ if not os.path.exists(dest):
  
 #populate ziplist list of unique shapefile root names by finding all files with .shp extension and removing extension
 for name in shps:
-  #prints full path for each shapefile
-  #print(name)
-  #retrieves just the files name for each name in shps
-  file = os.path.basename(name)
-  #removes .shp extension
-  names = file[:-4]
-  #adds each shapefile name to ziplist list
-  ziplist.append(names)
+	#prints full path for each shapefile
+	#print(name)
+	#retrieves just the files name for each name in shps
+	file = os.path.basename(name)
+	#removes .shp extension
+	names = file[:-4]
+	#adds each shapefile name to ziplist list
+	ziplist.append(names)
  
 #prints ziplist to confirm shapefile root names have been added
 #print(ziplist)
 
 #creates zipefiles in dest folder with basenames
 for f in ziplist:
-  # prints each item in the ziplist
-  #print(f)
-  #creates the name for each zipefile based on shapefile root names
-  file_name = os.path.join(dest, f+".zip")
-  #print to confirm
-  #print(file_name)
+	# prints each item in the ziplist
+	#print(f)
+	#creates the name for each zipefile based on shapefile root names
+	file_name = os.path.join(dest, f+".zip")
+	#print to confirm
+	#print(file_name)
 
-  #created the zipfiles with names defined above
-  zips = ZipFile(file_name, "w")
-  #print(zips)
-  #files lists all files with the current basename (f) from ziplist
-  files = glob.glob(str(f)+".*")
-  # iterate through each basename and add all shapefile components to the zipefile
-  for s in files:
-    #print(s)
-    zips.write(s)
-  zips.close()
+	#created the zipfiles with names defined above
+	zips = ZipFile(file_name, "w")
+	#print(zips)
+	#files lists all files with the current basename (f) from ziplist
+	files = glob.glob(str(f)+".*")
+	# iterate through each basename and add all shapefile components to the zipefile
+	for s in files:
+		#print(s)
+		zips.write(s)
+	zips.close()
