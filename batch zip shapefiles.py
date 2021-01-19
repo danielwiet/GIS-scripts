ba36b1c201391b19cad8bf04a4a948cb3019fa69
Daniel Wiet <danwiet@gmail.com>
2021-01-19 16:25:37 -0600

    add correct folder paths and syntax to make work

--- batch zip shapefiles.py
+++ batch zip shapefiles.py
@@ -1,70 +1,73 @@
+# updated by dan wiet on 01/19/2121
 # brian greer
-#4/2015
-#directory of shapefiles to zip for zipping individual shapefiles
+# 4/2015
+# directory of shapefiles to zip for zipping individual shapefiles
  
 #import modules needed
 import os
 import glob
 from zipfile import *
 
-#define location of shapefiles and destination of zipped shapefiles
-source = r"/Users/danwiet/Desktop/coding/esri/past 4 months/20201116_HC"
-dest = r"/Users/danwiet/Desktop/coding/esri/past 4 months/20201116_HC/zip"
+# define location of shapefiles and destination of zipped shapefiles
+# source = r"/Users/danwiet/Desktop/coding/esri/past 4 months/20201116_HC"
+#source = (path + "/" folder)
+# dest = r"/Users/danwiet/Desktop/coding/esri/past 4 months/20201116_HC/zip"
 
-# try 
-# for folders in os.listdir(path):
-# or
-# directory = os.listdir(path)
-# for folders in directory:
+path = input("What is the path to the parent folder: ")
+# /Users/danwiet/Desktop/coding/esri/past 4 months
 
-#change the current directory
-os.chdir(source)
- 
-#test current directory
-retval = os.getcwd()
-print(retval)
+for folders in os.listdir(path):
+	if not folders.startswith('.'):
+		# print(folders)
 
-#list all files with extension .shp
-shps = glob.glob(source+"/*.shp")
-#print(shps)
+		#change the current directory
+		os.chdir(path + "/" + folders)
+		
+		#test current directory
+		retval = os.getcwd()
+		print(retval)
 
-# create empty list for zipfile names
-ziplist = []
- 
-# create destination directory if it does not exist
-if not os.path.exists(dest):
-    os.makedirs(dest)
- 
-#populate ziplist list of unique shapefile root names by finding all files with .shp extension and removing extension
-for name in shps:
-	#prints full path for each shapefile
-	#print(name)
-	#retrieves just the files name for each name in shps
-	file = os.path.basename(name)
-	#removes .shp extension
-	names = file[:-4]
-	#adds each shapefile name to ziplist list
-	ziplist.append(names)
- 
-#prints ziplist to confirm shapefile root names have been added
-#print(ziplist)
+		# create destination directory if it does not exist
+		if not os.path.exists(retval + "/zip"):
+			os.makedirs(retval + "/zip")
 
-#creates zipefiles in dest folder with basenames
-for f in ziplist:
-	# prints each item in the ziplist
-	#print(f)
-	#creates the name for each zipefile based on shapefile root names
-	file_name = os.path.join(dest, f+".zip")
-	#print to confirm
-	#print(file_name)
+		# #list all files with extension .shp
+		shps = glob.glob(retval + "/*.shp")
+		# #print(shps)
 
-	#created the zipfiles with names defined above
-	zips = ZipFile(file_name, "w")
-	#print(zips)
-	#files lists all files with the current basename (f) from ziplist
-	files = glob.glob(str(f)+".*")
-	# iterate through each basename and add all shapefile components to the zipefile
-	for s in files:
-		#print(s)
-		zips.write(s)
-	zips.close()
+		# # create empty list for zipfile names
+		ziplist = []
+		
+		# #populate ziplist list of unique shapefile root names by finding all files with .shp extension and removing extension
+		for name in shps:
+		# 	#prints full path for each shapefile
+		# 	#print(name)
+		# 	#retrieves just the files name for each name in shps
+			file = os.path.basename(name)
+		# 	#removes .shp extension/Users/danwiet/Desktop/coding/esri/past 4 months 
+			names = file[:-4]
+		# 	#adds each shapefile name to ziplist list
+			ziplist.append(names)
+		
+		# #prints ziplist to confirm shapefile root names have been added
+		# #print(ziplist)
+
+		# #creates zipefiles in dest folder with basenames
+		for f in ziplist:
+		# 	# prints each item in the ziplist
+		#	# print(f)
+		# 	#creates the name for each zipefile based on shapefile root names
+			file_name = os.path.join(retval + "/zip", f+".zip")
+		# 	#print to confirm
+		# 	#print(file_name)
+
+		# 	#created the zipfiles with names defined above
+			zips = ZipFile(file_name, "w")
+		# 	#print(zips)
+		# 	#files lists all files with the current basename (f) from ziplist
+			files = glob.glob(str(f)+".*")
+		# 	# iterate through each basename and add all shapefile components to the zipefile
+			for s in files:
+				#print(s)
+				zips.write(s)
+			zips.close()
