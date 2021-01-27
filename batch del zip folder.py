26e30a848554e98cacd994ecbffed3e24a5a9d60
Daniel Wiet <danwiet@gmail.com>
2021-01-27 09:19:24 -0600

    add batch del zip folder using shutil

---
+++ batch del zip folder.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+path = input("What is the path to the directory you want to delete from?: ")
+
+for folders in os.listdir(path):
+	# ignore hidden files
+	if not folders.startswith('.'):
+
+		# change the current directory
+		os.chdir(path + "/" + folders)
+
+		# stores current directory pathname
+		parent_folder = os.getcwd()
+		# print(parent_folder)
+
+		# deletes the zipped folder
+		if os.path.exists(parent_folder + '/zipped_' + folders + '_shapefiles'):
+			shutil.rmtree(parent_folder + '/zipped_' + folders + '_shapefiles')
